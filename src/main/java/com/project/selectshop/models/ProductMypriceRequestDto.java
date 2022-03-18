6deb2dc253ba8b2a6fd3f4076b3fae58ef332c38
pypygeek <pypygeek@gmail.com>
2022-03-18 14:03:44 +0900

    Add first commit

---
+++ src/main/java/com/project/selectshop/models/ProductMypriceRequestDto.java
@@ -0,0 +1,8 @@
+package com.project.selectshop.models;
+
+import lombok.Getter;
+
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
---
+++ src/main/java/com/project/selectshop/models/ProductRequestDto.java
@@ -0,0 +1,11 @@
+package com.project.selectshop.models;
+
+import lombok.Getter;
+
+@Getter
+public class ProductRequestDto {
+    private String title;
+    private String link;
+    private String image;
+    private int lprice;
+}
